011e6a9703bb17493a111e82c9b22d1646874b8b
AkshataPShevalakar <akshataps1998.xworkz@gmail.com>
2021-09-29 13:51:42 +0530

    add

---
+++ Bike.java
@@ -0,0 +1,12 @@
+class Bike{
+public static void Details(String Company,float Price,String Color,int rcno,String name,int RegNo,String kg)
+{
+System.out.println(Company);
+System.out.println(Price);
+System.out.println(Color);
+System.out.println(rcno);
+System.out.println(name);
+System.out.println(RegNo);
+System.out.println(kg);
+}
+}
